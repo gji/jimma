e783923c7d944173f58fca95f5fdc8ccf0c7ee7f
Geoffrey Ji <geoffreyji@gmail.com>
2015-04-11 14:37:53 -0400

    added post bean

---
+++ backend/src/main/java/blue/jimma/backend/Post.java
@@ -0,0 +1,14 @@
+package blue.jimma.backend;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by gji on 4/11/15.
+ */
+public class Post {
+    BufferedImage b;
+    String comment;
+
+    int id;
+    
+}
